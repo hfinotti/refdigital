f3458717f1bfee8d208dfeaf9035fb410ba6270f
hfinotti <hfinotti@gmail.com>
2026-01-16 20:24:06 -0300

    Mudanca banco postgresql

--- refdigital/__init__.py
+++ refdigital/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///refdigital.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = "tlzOLzw7lax4nOLorzNJiJG8uhpumgKx"
 
 database = SQLAlchemy(app)
