c5085c0d3645668acf99135cb8dd3ef6e508871e
hfinotti <hfinotti@gmail.com>
2026-02-02 15:55:47 -0300

    Publicando a versao 1.3

--- refdigital/__init__.py
+++ refdigital/__init__.py
@@ -7,9 +7,9 @@ import os
 app = Flask(__name__)
 
 #if os.getenv("DATABASE_URL"):
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 #else:
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///refdigital.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///refdigital.db'
 
 app.config['SECRET_KEY'] = "tlzOLzw7lax4nOLorzNJiJG8uhpumgKx"
 
